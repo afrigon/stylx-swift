fc556c1ef1c3a3b60f99e80f6db785af7f74f8a5
Alexandre Frigon <alexandre.frigon.1@gmail.com>
2024-03-03 00:55:14 -0500

    public stuff

--- Sources/Debug/FontView.swift
+++ Sources/Debug/FontView.swift
@@ -1,12 +1,16 @@
 import SwiftUI
 
-struct FontView: View {
+public struct FontView: View {
     let value = "The quick brown fox jumps over the lazy dog"
     let weights: [Font.Weight] = [.black, .heavy, .bold, .semibold, .medium, .regular, .light, .thin, .ultraLight]
     
     let font: (Font.Weight) -> Font
     
-    var body: some View {
+    public init(font: @escaping (Font.Weight) -> Font) {
+        self.font = font
+    }
+    
+    public var body: some View {
         VStack {
             ForEach(weights, id: \.value) { weight in
                 Text(verbatim: value)
