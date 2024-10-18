4035e6dc1ab1f06c5a2d61e76dc1ee423961e27a
Alexandre Frigon <alexandre.frigon.1@gmail.com>
2024-10-18 02:44:26 -0400

    animation files

---
+++ Sources/Foundation/Animation/AnimationType.swift
@@ -0,0 +1,69 @@
+import SwiftUI
+
+public enum AnimationType {
+    case linear
+    case easeIn
+    case easeOut
+    case easeInOut
+    case bouncy
+
+    public func animation(duration: Double) -> Animation {
+        switch self {
+            case .linear:
+                .linear(duration: duration)
+            case .easeIn:
+                .easeIn(duration: duration)
+            case .easeOut:
+                .easeOut(duration: duration)
+            case .easeInOut:
+                .easeInOut(duration: duration)
+            case .bouncy:
+                .bouncy(duration: duration)
+        }
+    }
+}
+
+extension Animation {
+
+    @MainActor
+    public static func linear(_ speed: AnimationSpeed) -> Animation {
+        .linear(duration: speed.rawValue)
+    }
+
+    @MainActor
+    public static func easeIn(_ speed: AnimationSpeed) -> Animation {
+        .easeIn(duration: speed.rawValue)
+    }
+
+    @MainActor
+    public static func easeOut(_ speed: AnimationSpeed) -> Animation {
+        .easeOut(duration: speed.rawValue)
+    }
+
+    @MainActor
+    public static func easeInOut(_ speed: AnimationSpeed) -> Animation {
+        .easeInOut(duration: speed.rawValue)
+    }
+
+    @MainActor
+    public static func bouncy(_ speed: AnimationSpeed) -> Animation {
+        .bouncy(duration: speed.rawValue)
+    }
+
+    @MainActor
+    public static func animate(
+        _ animation: AnimationType,
+        duration: AnimationSpeed = .default,
+        _ body: () -> Void
+    ) async {
+        await withCheckedContinuation { continuation in
+            withAnimation(animation.animation(duration: duration.rawValue)) {
+                body()
+            }
+
+            DispatchQueue.main.asyncAfter(deadline: .now() + duration.rawValue) {
+                continuation.resume()
+            }
+        }
+    }
+}
