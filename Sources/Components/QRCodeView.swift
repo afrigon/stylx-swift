438a987e8687a87fe2e02a57b7814ea8aa73d669
Alexandre Frigon <alexandre.frigon.1@gmail.com>
2024-10-18 01:15:36 -0400

    tweaks

--- Sources/Components/QRCodeView.swift
+++ Sources/Components/QRCodeView.swift
@@ -1,29 +1,28 @@
 import SwiftUI
-import stylx
 import CoreImage.CIFilterBuiltins
 
 public struct QRCodeView: View {
-    @State var image: PlatformImage?
-    
-    let data: Data
-    
+    @State private var image: PlatformImage?
+
+    private let data: Data
+
     public init(_ data: Data) {
         self.data = data
     }
-    
+
     public init?(_ data: String) {
         guard let data = data.data(using: .utf8) else {
             return nil
         }
-        
+
         self.data = data
     }
-    
+
     public init?(_ data: URL) {
         guard let data = data.absoluteString.data(using: .utf8) else {
             return nil
         }
-        
+
         self.data = data
     }
 
@@ -39,23 +38,23 @@ public struct QRCodeView: View {
                             .aspectRatio(contentMode: .fit)
                     }
                 }
-                .onAppear() {
+                .onAppear {
                     image = generate()
                 }
         }
     }
-    
+
     private func generate() -> PlatformImage? {
         let context = CIContext()
         let filter = CIFilter.qrCodeGenerator()
-        
+
         filter.message = data
-        
+
         guard let outputImage = filter.outputImage,
               let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else {
             return nil
         }
-        
+
         return PlatformImage(cgImage: cgImage, size: outputImage.extent.size)
     }
 }
